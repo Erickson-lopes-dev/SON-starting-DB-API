3d16a818e3df516140e9602f9c1f2bbe61ee8a5f
EricksonLopes <ofc.erickson@gmail.com>
2020-09-08 10:04:14 -0300

    update

--- main.py
+++ main.py
@@ -22,16 +22,33 @@ db.commit()
 # db.rollback()
 
 cursor = db.cursor()
+
+cursor.execute("UPDATE pessoas SET nome='fernando', idade='10' WHERE id=6 ")
+
+db.commit()
+
 # executa o método dentro do banco
 row_effected = cursor.execute('SELECT * FROM pessoas')
 
 # exibe linhas afetadas
 print(row_effected)
 
-# quantidades de linhas para buscar
-rows_fet = cursor.fetchmany(1)
 
-print(rows_fet)
+def insert_item():
+    # inserir item
+    cursor.execute("INSERT INTO "
+                   "pessoas " "(nome, idade) "
+                   "VALUES ('Carlos', '24')")
+    # comita as alterações no banco
+    db.commit()
+
+
+# exibindo quatidade de linhas especificadas
+def get_fetchaamy():
+    # quantidades de linhas para buscar
+    rows_fet = cursor.fetchmany(1)
+
+    print(rows_fet)
 
 
 # buscando todas as linhas
@@ -45,6 +62,8 @@ def get_fetchall():
     for row in cursor:
         print(row)
 
+
+get_fetchall()
 
 # fecha banco
 db.close()
